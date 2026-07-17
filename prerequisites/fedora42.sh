17d5e7195c29665a64a2f91c436e7ff07582313c
Filippo Di Marco <filidm5795@gmail.com>
2026-07-17 16:31:59 +0200

    chore: update all provider, kind, matrics and kind linux versions

--- prerequisites/fedora42.sh
+++ prerequisites/fedora42.sh
@@ -33,7 +33,7 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # Scarica e installa Kind se l'architettura è x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.27.0/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.32.0/kind-linux-amd64
 
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
--- providers.tf
+++ providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     kind = {
       source = "tehcyx/kind"
-      version = "0.8.0"
+      version = "0.11.0"
     }
     local = {
       source = "hashicorp/local"
-      version = "~> 2.5"
+      version = "2.9.0"
     }
     helm = {
       source = "opentofu/helm"
-      version = "3.0.0-pre2"
+      version = "3.2.0"
     }
     kubectl = {
       source = "gavinbunney/kubectl"
